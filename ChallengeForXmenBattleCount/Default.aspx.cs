8581bc24f0ac223e59a1cf185980ee18af804c3f
reismahnic <reismahnic@gmail.com>
2017-07-26 00:31:42 -0700

    Base file commit
    Contains the base file I will be working with

---
+++ ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+
+            // Your Code Here!
+
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
